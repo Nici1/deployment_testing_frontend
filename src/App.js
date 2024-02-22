aa67886aca9f7e8aacb987f9e34402bc8d65ea0b
nikola <nicisekulovski@gmail.com>
2024-02-22 20:10:51 +0100

    4th commit

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
     <Router>
    
       <Routes>
-        <Route path="/" element={<TrialPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<TrialPage />} />
 
 
 
