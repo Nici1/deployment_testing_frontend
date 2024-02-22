f656f409080dafadd5c558d9ed5ebdb2388f497f
nikola <nicisekulovski@gmail.com>
2024-02-22 20:46:45 +0100

    4th commit

--- src/pages/TrialPage.js
+++ src/pages/TrialPage.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom'; // Import useHistory hook for navigation
 import baseUrl from '../baseUrl.js';
 
 function TrialPage() {
   const [response, setResponse] = useState('');
+  const history = useHistory(); // Initialize useHistory hook
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,11 +22,20 @@ function TrialPage() {
     fetchData();
   }, []);
 
+  const navigateToAnotherPage = () => {
+    // Navigate to another page when the button is clicked
+    history.push('/login'); // Specify the path of the target page
+  };
+
   return (
     <div>
       <p>Response from server: {response}</p>
+      
+      {/* Button to navigate to another page */}
+      <button onClick={navigateToAnotherPage}>Go to Another Page</button>
     </div>
   );
 }
 
 export default TrialPage;
+
